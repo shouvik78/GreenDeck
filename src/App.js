9e4dfed88f2b29a8715687bf1ff6b1957d8eec86
Shouvik <49680243+shouvik78@users.noreply.github.com>
2020-10-12 12:57:26 +0530

    First

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import "./App.css";
+import Navigation from "./Navigation/Navigation";
+import Home from "./Home/Home";
+import Particles from "react-particles-js";
+import Signin from "./Signin/Signin";
+import Register from "./Register/Register";
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 40,
+      density: {
+        enable: true,
+        value_area: 500,
+      },
+    },
+  },
+};
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      imageUrl: "",
+      box: {},
+      route: "signin",
+      isSignedIn: false,
+    };
+  }
+
+  onRouteChange = (route) => {
+    if (route === "signout") {
+      this.setState({ isSignedIn: false });
+    } else if (route === "home") {
+      this.setState({ isSignedIn: true });
+    }
+    this.setState({ route: route });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles" params={particlesOptions} />
+
+        <Navigation
+          isSignedIn={this.state.isSignedIn}
+          onRouteChange={this.onRouteChange}
+        />
+        {this.state.route === "home" ? (
+          <div>
+            <Home />
+          </div>
+        ) : this.state.route === "signin" ? (
+          <Signin onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register onRouteChange={this.onRouteChange} />
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Home/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="white f3">{"Welcome..."}</div>
+    </div>
+  );
+};
+export default Home;
